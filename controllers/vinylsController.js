a4dd908f9ef2862fd863cee7c56d8542a38d0316
MicFinns <mrfinneran+meta@gmail.com>
2022-03-01 11:54:04 -0800

    add middleware so express can accept json data

--- controllers/vinylsController.js
+++ controllers/vinylsController.js
@@ -4,7 +4,7 @@ const Vinyl = require('./../models/Vinyl')
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    Vinyl.find()
+    Vinyl.find({})
         .then((vinyls => res.json({
             status: 200,
             vinyls: vinyls
