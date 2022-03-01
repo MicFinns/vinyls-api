7d7beaa8b325eff6679222332d7135c54aa457de
MicFinns <mrfinneran+meta@gmail.com>
2022-03-01 11:17:16 -0800

    add vinyls controller and get /vinyls route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const vinylsController = require('./controllers/vinylsController')
 
 const PORT = process.env.PORT
 
@@ -9,9 +10,7 @@ const app = express()
 app.use(logger('dev'));
 app.use(cors())
 
-app.get('/', function (req, res) {
-    res.send('hello, earth!')
-})
+app.use('/vinyls', vinylsController)
 
 app.listen(PORT, () => {
     console.log(`Listening in on port: ${PORT}`)
